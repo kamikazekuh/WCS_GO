2c5d80668c4fcbdfb33b5b56a418c9b7848efda8
kamikazekuh <33634496+kamikazekuh@users.noreply.github.com>
2017-12-30 09:45:17 +0100

    Update downloader.py

--- addons/source-python/plugins/wcs/downloader.py
+++ addons/source-python/plugins/wcs/downloader.py
@@ -1,20 +1,22 @@
 from stringtables.downloads import Downloadables
 from paths import GAME_PATH
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, isdir
 from engines.precache import Model
 
 downloads = Downloadables()
 
 paths_to_add = [GAME_PATH+'/materials/sprites', GAME_PATH+'/materials/particle', GAME_PATH+'/materials/effects']
 
-
 for mypath in paths_to_add:
-	downloads.add_directory(mypath)
-	for mypath in paths_to_add:
-		files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-		for x in files:
-			Model(x,True)
+	if isdir(mypath):
+		downloads.add_directory(mypath)
+		for mypath in paths_to_add:
+			if isdir(mypath):
+				files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+				for x in files:
+					Model(x,True)
+	
 
 
 
