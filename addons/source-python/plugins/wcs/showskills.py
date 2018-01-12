3200060dee70d91f04d5ba35b06fc6f7981e4014
kamikazekuh <33634496+kamikazekuh@users.noreply.github.com>
2018-01-13 00:52:22 +0100

    changed the menu visuals and added a back button

--- addons/source-python/plugins/wcs/showskills.py
+++ addons/source-python/plugins/wcs/showskills.py
@@ -2,7 +2,9 @@ from players.helpers import index_from_userid, playerinfo_from_userid, index_fro
 from menus import SimpleMenu
 from menus import SimpleOption
 from menus import Text
+from players.entity import Player
 import wcs
+import core
 
 def doCommand(userid):
 	index = index_from_userid(userid)
@@ -11,9 +13,9 @@ def doCommand(userid):
 	skills = player.race.skills.split('|')
 	names = player.race.racedb['skillnames'].split('|')
 	n = 0
-	showskills = SimpleMenu()
+	showskills = SimpleMenu(select_callback=showskills_select)
 	showskills.append(Text('Your %s skills:' % player.player.currace))
-	showskills.append(Text(' '))
+	showskills.append(Text('------------------------------'))
 	while n < len(names):
 		try:
 			level = str(skills[n])
@@ -22,8 +24,15 @@ def doCommand(userid):
 		name = str(names[n])
 		n += 1
 		showskills.append(SimpleOption(n, '%s : %s' % (str(name), str(level)), value=None, highlight=True, selectable=False))
-	showskills.append(Text(' '))
-	showskills.append(Text(' '))
+	showskills.append(Text('------------------------------'))
 	showskills.append(Text('Unused points: %s' % str(player.race.unused)))
+	showskills.append(Text('------------------------------'))
+	showskills.append(SimpleOption(7, 'Back',value='back',highlight=True,selectable=True))
 	showskills.append(SimpleOption(9, 'Close', highlight=False))
 	showskills.send(index)
+
+	
+def showskills_select(menu, index, choice):
+	if choice.choice_index == 7:
+		player=Player(index)
+		player.client_command('wcs',server_side=True)
