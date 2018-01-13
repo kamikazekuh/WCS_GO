557d4ef5e25374386a44ed5f149dfec5b3b6d582
kamikazekuh <33634496+kamikazekuh@users.noreply.github.com>
2018-01-13 14:14:44 +0100

    fix for gravity and ladder

---
+++ addons/source-python/plugins/wcs/ladderfix.py
@@ -0,0 +1,36 @@
+from listeners import OnTick
+from players.entity import Player
+from filters.players import PlayerIter
+from events import Event
+from wcs import wcsgroup
+
+
+ladder = {}
+
+
+@Event('player_activate')
+def player_activate(ev):
+	ladder[ev['userid']] = 2
+
+@OnTick
+def tick_listener():
+	for player in PlayerIter('all'):
+		newval = player.move_type
+		if player.userid in ladder:
+			oldval = ladder[player.userid]
+		else:
+			ladder[player.userid] = {}
+			oldval = 2		
+		if oldval != newval:
+			if newval == 2:
+				if oldval == 9:
+					if wcsgroup.getUser(player.userid,'gravity') != None:
+						gravity = wcsgroup.getUser(player.userid, 'gravity')
+					else:
+						gravity = 1.0
+					player.gravity = float(gravity)
+					jetpack = int(wcsgroup.getUser(player.userid, 'jetpack'))
+					if jetpack == 1:
+						player.set_jetpack(1)
+		ladder[player.userid] = newval
+		
