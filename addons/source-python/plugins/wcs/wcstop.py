ea15403a59020d5c7b28d3b34aa10af89609bcce
kamikazekuh <33634496+kamikazekuh@users.noreply.github.com>
2018-02-02 17:10:50 +0100

    Fixed error due to missing SQL Alchemy support

--- addons/source-python/plugins/wcs/wcstop.py
+++ addons/source-python/plugins/wcs/wcstop.py
@@ -1,4 +1,5 @@
 import wcs
+from wcs.database import database
 from menus import PagedMenu
 from players.entity import Player
 from players.helpers import index_from_userid, playerinfo_from_userid, index_from_playerinfo, userid_from_index
@@ -8,28 +9,32 @@ from menus import PagedOption
 from menus import Text
 
 def wcstop_menu_select(menu, index, choice):
-    menu.close()
+	menu.close()
 
 def wcstop_menu_build(menu, index):
-    menu.clear()
-    allplayers = list(wcs.wcs.database.ranks)
-    if len(allplayers):
-        for number, steamid in enumerate(allplayers):
-            if number < 5:
-                wcs.wcs.database.execute("SELECT UserID, name, totallevel, currace FROM Players WHERE steamid = ?", (steamid, ))
-                v = wcs.wcs.database.fetchall()
-                if v:
-                    UserID, name, totallevel, currace = v[0]
+	menu.clear()
+	allplayers = list(wcs.wcs.database.ranks)
+	if len(allplayers):
+		for number, steamid in enumerate(allplayers):
+			if number < 5:
+				UserID = database.getUserIdFromSteamId(steamid)
+				v = database.getInfoPlayer('all',UserID)
+				if v:
+					name = v[2]
+					currace = v[1]
+					totallevel = v[3]
+					RaceID = database.getRaceIdFromUserIdAndRace(UserID,currace)
+					z = database.getInfoRace('all',UserID,RaceID)
+					if z:
+						level = z[2]
 
-                    wcs.wcs.database.execute("SELECT level FROM Races WHERE UserID = ? AND name = ?", (UserID, currace))
-                    level = wcs.wcs.database.fetchone()
-                    option = PagedOption('%s | Total level: %s | Playing %s | Level %s' % (str(name),totallevel, currace, level), str(name), highlight=True, selectable=False)
-                    menu.append(option)
-                    #menu.append(Text('  Total level: %s | Playing %s | Level %s' % (totallevel, currace, level)))
-                    
+					option = PagedOption('%s | Total level: %s | Playing %s | Level %s' % (str(name),totallevel, currace, level), str(name), highlight=True, selectable=False)
+					menu.append(option)
+					#menu.append(Text('	 Total level: %s | Playing %s | Level %s' % (totallevel, currace, level)))
+					
 def doCommand(userid):
-    wcstop_menu = PagedMenu(title='WCS Top Menu',build_callback=wcstop_menu_build, select_callback=wcstop_menu_select)
-    wcstop_menu.send(index_from_userid(userid))
+	wcstop_menu = PagedMenu(title='WCS Top Menu',build_callback=wcstop_menu_build, select_callback=wcstop_menu_select)
+	wcstop_menu.send(index_from_userid(userid))
 
 def wcsRank(userid):
 	player = wcs.wcs.getPlayer(userid)
