774c21d1676db7d43f1155e8762cbd1ba9ec17b1
kamikazekuh <33634496+kamikazekuh@users.noreply.github.com>
2018-02-03 12:40:45 +0100

    Fixed ultimates to work properly

--- addons/source-python/plugins/es_emulator/eventscripts/wcs/tools/ultimates/default2/default2.py
+++ addons/source-python/plugins/es_emulator/eventscripts/wcs/tools/ultimates/default2/default2.py
@@ -1,123 +1,78 @@
 import es
-import gamethread
-import playerlib
-import usermsg
-import random
-import vecmath
-from wcs import wcs
+from listeners.tick import Delay
+from players.entity import Player
+from filters.players import PlayerIter
+from colors import Color
+from wcs import wcs_commands
+from wcs import wcsgroup
+from mathlib import Vector
+from engines.server import queue_command_string
+import core
 
-def wcs_ulti_teleport():
-	userid = int(es.ServerVar('wcs_userid'))
+def remove_freeze(player):
+	player.freeze = 0
 
-	if es.getplayerteam(userid) >= 2:
-		if playerlib.getUseridList('#alive'):
-			player = playerlib.getPlayer(userid)
+def cancel(userid, what):
+	wcsgroup.setUser(userid, what+'_cooldown', wcsgroup.getUser(userid, what+'_pre_cooldown'))
 
-			vector1 = vecmath.Vector(es.getplayerlocation(userid))
-			vector2 = vecmath.Vector(player.getViewCoord())
-
-			if vecmath.distance(vector1, vector2) <= float(es.ServerVar('wcs_range')):
-				es.server.queuecmd("wcs_pushto %s %s %s %s 1.5" % (userid, vector2[0],vector2[1],vector2[2]))
-				es.tell(userid, '#multi', '#lightgreenYou Teleported!')
-				es.playsound(userid, 'wcs/teleport.wav', 0.5)
-			else:
-				es.server.queuecmd("wcs_cancelulti %s" % (userid))
-				es.tell(userid, '#multi', '#lightgreenSorry, you cannot teleport that far!')
 
 def wcs_ulti_roots():
 	userid = int(es.ServerVar('wcs_userid'))
-	count = 0
-
-	if es.getplayerteam(userid) >= 2:
-		if playerlib.getUseridList('#alive'):
-			usermsg.fade(userid, 0, 1, 1, 10, 55, 5, 200)
-			x,y,z = es.getplayerlocation(userid)
-			radius = float(es.ServerVar('wcs_radius'))
-			time = float(es.ServerVar('wcs_duration'))
-
-			for user in playerlib.getUseridList('#alive,#'+['ct','t'][es.getplayerteam(userid)-2]):
-				x1,y1,z1 = es.getplayerlocation(user)
-
-				if ((x1 - x) ** 2 + (y1 - y) ** 2 + (z1 - z) ** 2) ** 0.5 <= radius:
-
-					if not wcs.wcsgroup.getUser(user, 'ulti_immunity'):
-						playerlib.getPlayer(user).freeze = 1
-						es.server.queuecmd('est_effect_06 #a 0 sprites/laser.vmt '+str(Vector(x,y,z+35))+' '+str(Vector(x1,y1,z1+35))+' 10 1 5 5 10 3.8 0 160 0 155 2')
-						es.server.queuecmd('est_effect_06 #a 0 sprites/laser.vmt '+str(Vector(x,y,z+35))+' '+str(Vector(x1,y1,z1+35))+' 10 1 4 4 10 2.3 0 108 0 180 2')
-						es.server.queuecmd('est_effect_06 #a 0 sprites/laser.vmt '+str(Vector(x,y,z+35))+' '+str(Vector(x1,y1,z1+35))+' 10 1 5 5 10 4.1 0 80 0 190 2')
-						es.server.queuecmd('est_effect 10 #a 0 sprites/laser.vmt '+str(x)+' '+str(y)+' '+str(z+10)+' 60 20 1 3 3 5 0 80 0 155 0')
-						es.server.queuecmd('est_effect 10 #a 0 sprites/laser.vmt '+str(x)+' '+str(y)+' '+str(z+25)+' 75 35 1 3 3 6 0 108 0 190 0')
-						es.emitsound('player', user, 'wcs/root.wav', 1.0, 0.6)
-						gamethread.delayed(time, reset, (user, 'freeze', 0))
+	player = Player.from_userid(userid)
+	if player.team >= 2:
+		count = 0
+		wcs_commands.fade(userid,10,55,5,200,1)
+		radius = float(es.ServerVar('wcs_radius'))
+		time = float(es.ServerVar('wcs_duration'))
+		atk_origin = player.origin
+		for play in PlayerIter('alive'):
+			if play.team != player.team:
+				vic_origin = play.origin
+				if vic_origin.get_distance(atk_origin) <= radius:
+					if not wcsgroup.getUser(play.userid, 'ulti_immunity'):
+						play.freeze = 1
 						count += 1
-
+						Delay(time, remove_freeze, (play,))
+						es.emitsound('player', play.userid, 'wcs/root.wav', 1.0, 0.6)
+						queue_command_string('es est_effect_06 #a 0 sprites/laser.vmt "%s,%s,%s" "%s,%s,%s" 10 1 5 5 10 4 0 160 0 155 2' % (atk_origin[0],atk_origin[1],atk_origin[2]+35,vic_origin[0],vic_origin[1],vic_origin[2]+35))
+						queue_command_string('es est_effect_06 #a 0 sprites/laser.vmt "%s,%s,%s" "%s,%s,%s" 10 1 4 4 10 2 0 108 0 180 2' % (atk_origin[0],atk_origin[1],atk_origin[2]+35,vic_origin[0],vic_origin[1],vic_origin[2]+35))
+						queue_command_string('es est_effect_06 #a 0 sprites/laser.vmt "%s,%s,%s" "%s,%s,%s" 10 1 5 5 10 4 0 80 0 190 2' % (atk_origin[0],atk_origin[1],atk_origin[2]+35,vic_origin[0],vic_origin[1],vic_origin[2]+35))
+						queue_command_string('es est_effect 10 #a 0 sprites/laser.vmt %s %s %s 60 20 1 3 3 5 0 80 0 155 0 255 2' % (vic_origin[0],vic_origin[1],vic_origin[2]+10))
+						queue_command_string('es est_effect 10 #a 0 sprites/laser.vmt %s %s %s 75 35 1 3 3 6 0 108 0 190 0 255 2' % (vic_origin[0],vic_origin[1],vic_origin[2]+25))
 					else:
-						wcs.tell(user, 'u_ulti_immunity_v')
-						wcs.tell(userid, 'u_ulti_immunity_a')
-
-	if count:
-		es.centertell(wcs.strings('c_u_entanglingroots', {'count':count}, playerlib.getPlayer(userid).lang))
+						es.tell(player.userid, '#multi', '#lightgreenYour ultimate was blocked, the enemy is #greenimmune.')
+						es.tell(play.userid, '#multi', '#lightgreenYou #greenblocked #lightgreenan ultimate skill.')
+	if count > 0:
+		es.centertell(userid, 'Entangling Roots: %s' % (count))
 	else:
-		wcs.tell(userid, 'u_roots_failed')
-		wcs.cancel(userid, 'player_ultimate')
-
+		es.tell(userid, '#multi','#lightgreenEntangling Roots #greenfailed#lightgreen, because no enemy is close enough.')
+		cancel(userid,'player_ultimate')
+			
+			
 def wcs_ulti_chain():
 	userid = int(es.ServerVar('wcs_userid'))
-	count = 0
-
-	if es.getplayerteam(userid) >= 2:
-		if playerlib.getUseridList('#alive'):
-			usermsg.fade(userid, 0, 2, 1, 240, 240, 240, 100)
-			x,y,z = es.getplayerlocation(userid)
-			radius = float(es.ServerVar('wcs_radius'))
-
-			for user in playerlib.getUseridList('#alive,#'+['ct','t'][es.getplayerteam(userid)-2]):
-				x1,y1,z1 = es.getplayerlocation(user)
-
-				if ((x1 - x) ** 2 + (y1 - y) ** 2 + (z1 - z) ** 2) ** 0.5 <= radius:
-
-					if not wcs.wcsgroup.getUser(user, 'ulti_immunity'):
-						es.server.queuecmd("wcs_dealdamage %s %s 32" % (user, userid))
-						es.server.queuecmd('est_effect_06 #a 0 sprites/lgtning.vmt '+str(Vector(x,y,z+35))+' '+str(Vector(x1,y1,z1+35))+' 10 .2 10 1 1 13 160 160 230 255 11')
-						es.server.queuecmd('est_effect_06 #a 0 sprites/lgtning.vmt '+str(Vector(x,y,z+35))+' '+str(Vector(x1,y1,z1+35))+' 10 .2 10 2 2 12 150 150 255 220 8')
-						es.server.queuecmd('est_effect_06 #a 0 sprites/lgtning.vmt '+str(Vector(x,y,z+35))+' '+str(Vector(x1,y1,z1+35))+' 10 .2 10 1 1 14 200 200 255 235 10')
-						es.emitsound('player', user, 'wcs/lightning.wav', 1.0, 0.6)
+	player = Player.from_userid(userid)
+	if player.team >= 2:
+		count = 0
+		wcs_commands.fade(userid,10,55,5,200,1)
+		radius = float(es.ServerVar('wcs_radius'))	
+		atk_origin = player.origin
+		for play in PlayerIter('alive'):
+			if play.team != player.team:
+				vic_origin = play.origin
+				if vic_origin.get_distance(atk_origin) <= radius:
+					if not wcsgroup.getUser(play.userid, 'ulti_immunity'):
+						wcs_commands.damage(play.userid,32,userid,solo=1)
 						count += 1
-
+						es.emitsound('player', play.userid, 'wcs/lightning.wav', 1.0, 0.6)
+						queue_command_string('es est_effect_06 #a 0 sprites/lgtning.vmt "%s,%s,%s" "%s,%s,%s" 10 .2 10 1 1 13 160 160 230 255 11' % (atk_origin[0],atk_origin[1],atk_origin[2]+35,vic_origin[0],vic_origin[1],vic_origin[2]+35))
+						queue_command_string('es est_effect_06 #a 0 sprites/lgtning.vmt "%s,%s,%s" "%s,%s,%s" 10 .2 10 2 2 12 150 150 255 220 8' % (atk_origin[0],atk_origin[1],atk_origin[2]+35,vic_origin[0],vic_origin[1],vic_origin[2]+35))
+						queue_command_string('es est_effect_06 #a 0 sprites/lgtning.vmt "%s,%s,%s" "%s,%s,%s" 10 .2 10 1 1 14 200 200 255 235 10' % (atk_origin[0],atk_origin[1],atk_origin[2]+35,vic_origin[0],vic_origin[1],vic_origin[2]+35))
 					else:
-						wcs.tell(user, 'u_ulti_immunity_v')
-						wcs.tell(userid, 'u_ulti_immunity_a')
-
-	if count:
-		es.centertell(wcs.strings('c_u_chainglightning', {'count':count}, playerlib.getPlayer(userid).lang))
+						es.tell(userid, '#multi', '#lightgreenYour ultimate was blocked, the enemy is #greenimmune.')
+						es.tell(play.userid, '#multi', '#lightgreenYou #greenblocked #lightgreenan ultimate skill.')
+	if count > 0:
+		es.centertell(userid, 'Chain Lightning: %s players damaged' % (count))
 	else:
-		wcs.tell(userid, 'u_chain_failed')
-		wcs.cancel(userid, 'player_ultimate')
-
-def wcs_ulti_suicide():
-	userid = int(es.ServerVar('wcs_userid'))
-
-	if es.getplayerteam(userid) >= 2:
-		if playerlib.getUseridList('#alive'):
-			x,y,z = es.getplayerlocation(userid)
-			radius = float(es.ServerVar('wcs_radius'))
-			magnitude = float(es.ServerVar('wcs_magnitude'))
-			v = round(radius * magnitude) / 150
-			es.server.queuecmd('est_effect_20 #a 0 sprites/lgtning.vmt '+str(Vector(x,y,z+35))+' 1 10 5 200 10 '+str(x)+' '+str(y)+' '+str(z+10)+' metal')
-
-			for user in playerlib.getUseridList('#alive,#'+['ct','t'][es.getplayerteam(userid)-2]):
-				x1,y1,z1 = es.getplayerlocation(user)
-
-				if ((x1 - x) ** 2 + (y1 - y) ** 2 + (z1 - z) ** 2) ** 0.5 <= radius:
-
-					if not wcs.wcsgroup.getUser(user, 'ulti_immunity'):
-						es.server.queuecmd("wcs_dealdamage %s %s %s" % (user, userid, v))
-						es.emitsound('player', userid, 'ambient/explosions/explode_5.wav', 1.0, 0.6)
-
-					else:
-						wcs.tell(user, 'u_ulti_immunity_v')
-						wcs.tell(userid, 'u_ulti_immunity_a')
-
-def reset(userid, what, default):
-	if es.exists('userid', userid):
-		setattr(playerlib.getPlayer(userid), what, default)
+		es.tell(userid, '#multi', '#lightgreenChain Lightning #greenfailed#lightgreen, because no enemy is close enough to be damaged.')
+		cancel(userid,'player_ultimate')
