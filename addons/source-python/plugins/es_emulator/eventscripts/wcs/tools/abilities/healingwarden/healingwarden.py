7f7a1e90fb1beb2b3e0c89bd008321684825a0d6
kamikazekuh <33634496+kamikazekuh@users.noreply.github.com>
2018-06-17 02:36:56 +0200

    added healing ward

---
+++ addons/source-python/plugins/es_emulator/eventscripts/wcs/tools/abilities/healingwarden/healingwarden.py
@@ -0,0 +1,36 @@
+import es
+from wcs import wcsgroup
+from wcs.xtell import tell
+
+
+def healingwarden():
+	userid = str(es.ServerVar('wcs_userid'))
+	count = int(wcsgroup.getUser(userid, 'ability_count'))
+
+	if count:
+		param = str(wcsgroup.getUser(userid, 'ability_parameter'))
+
+		if param:
+			param = param.split('_')
+			team = int(es.getplayerteam(userid))
+
+			if team == 2:
+				teamtarget = '2'
+				teamtargetn = '#t'
+				color = '255 0 10 150'
+
+			elif team == 3:
+				teamtarget = '3'
+				teamtargetn = '#ct'
+				color = '10 0 255 150'
+
+			x,y,z = es.getplayerlocation(userid)
+			es.server.queuecmd('wcs_healingwarden '+userid+' '+param[0]+' '+param[1]+' '+param[2]+' '+teamtarget+' '+teamtargetn+' '+str(x)+' '+str(y)+' '+str(z)+' '+str(es.ServerVar('wcs_roundcounter')))
+
+			tell(userid, 'a_wardencreated')
+
+		if count and not count == -1:
+			wcsgroup.setUser(userid, 'ability_count', count-1)
+
+	else:
+		tell(userid, 'a_failed')
